59c4c83184c3a76387f715951b4667583408c6cc
David Ly <lydawei@duck.com>
2026-07-22 11:16:39 -0500

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const dict = new Map();
+
+        for(let i = 0; i < nums.length; i++){
+            let value = nums[i];
+
+            let complement = target - value;
+
+
+
+            if(dict.has(complement)){
+                return [dict.get(complement), i];
+            }
+
+            dict.set(value, i);
+        }
+    }
+}
